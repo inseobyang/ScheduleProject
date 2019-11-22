140f89946cf89366373b483fb63fab506ff40907
wueddyw <wueddyw@gmail.com>
2019-11-21 16:04:15 -0800

    Big push from eddy

--- scripts/add-schedule.js
+++ scripts/add-schedule.js
@@ -1,7 +1,7 @@
 function addSchedule(courseName, courseBuilding, courseCode, courseNumber, courseRoom ){
     firebase.auth().onAuthStateChanged(function (user){
         
-            db.collection("Users").doc(user.displayName).collection("Courses").doc(courseName + " " + courseNumber ).set({
+            db.collection("Users").doc(user.uid).collection("Courses").add({
                 "Course" : [courseName, courseCode, courseNumber],
                 "CourseLocation" : [courseBuilding, courseRoom ]
         
